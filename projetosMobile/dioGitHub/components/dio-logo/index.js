0ef7630c57a2896ca1245719acc69dea15a1534c
Henrique de Rossi Windlin <hwindlin@gmail.com>
2022-06-19 17:41:05 -0300

    add final projects of Dio course

---
+++ projetosMobile/dioGitHub/components/dio-logo/index.js
@@ -0,0 +1,175 @@
+import React from "react";
+import {View} from "react-native";
+import style from "./style";
+
+const Dio = () => {
+    return (
+        <>
+        <View style={style.dotsRow}>
+            <View style={style.dot}>
+            </View>
+            <View style={style.dot}>
+            </View>
+            <View style={style.dot}>
+            </View>
+            <View style={style.dot}>
+            </View>
+            <View style={style.dot}>
+            </View>
+            <View style={style.dot}>
+            </View>
+            <View style={style.dot}>
+            </View>
+            <View style={style.dot}>
+            </View>
+            <View style={style.dot}>
+            </View>
+            <View style={style.dot}>
+            </View>
+            <View style={style.dot}>
+            </View>
+            <View style={style.dot}>
+            </View>
+            <View style={style.dot}>
+            </View>
+        </View><View style={style.dotsRow}>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+            </View><View style={style.dotsRow}>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+            </View><View style={style.dotsRow}>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+            </View><View style={style.dotsRow}>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={[style.dot, style.lighterDot]}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+            </View><View style={style.dotsRow}>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+                <View style={style.dot}>
+                </View>
+            </View>
+            </>
+    )
+};
+
+export default Dio;
